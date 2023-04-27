d8b9ac5f84a48b34e0f90e02288f78f3b7b0d097
jonathan esquivel <jonathan@MacBookAir.local>
2023-04-26 19:18:37 -0500

    Add solution

--- raindrops.js
+++ raindrops.js
@@ -1,1 +1,26 @@
-export function convert() {}
+const dividers = [
+  {
+    number: 3,
+    sound: "Pling",
+  },
+  {
+    number: 5,
+    sound: "Plang",
+  },
+  {
+    number: 7,
+    sound: "Plong",
+  },
+];
+
+export function convert(num) {
+  let result = [];
+
+  dividers.map((div) => {
+    num % div.number === 0 && result.push(div.sound);
+  });
+
+  if (result.length === 0) result.push(num);
+
+  return result.join("");
+}
